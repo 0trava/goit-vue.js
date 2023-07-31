aa9c9ea2e6a4a3f02ef117077c4e20c3a320ef8c
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-31 13:30:34 +0300

    add

--- work_file/src/components/Title.vue
+++ work_file/src/components/Title.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <h1>First title</h1>
-
+        <h1>First title</h1>
     </div>
 </template>
 
 <script>
     export default {
-        name: "titleFirst"
+        name: "titleGeneral"
     }
 </script>
 
